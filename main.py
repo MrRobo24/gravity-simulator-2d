41a96cdc235960878501d6eefcc8cbad4f334405
MrRobo24 <sumanarpit.24@gmail.com>
2025-04-03 02:48:56 +0530

    calc velocity and pos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-import time
+from body import Body
+from physics import Physics
 
 # Initialize pygame
 pygame.init()
@@ -17,6 +18,21 @@ WHITE = (255, 255, 255)
 circle_pos = (100, 100)  # (x, y) position
 circle_radius = 50
 
+first_body = Body(
+    name = "Earth",
+    mass = 10,  # Mass in kg
+    radius = circle_radius,  # Radius in pixels
+    color = WHITE,  # Color in RGB
+    velocity = (0, 0),  # Initial velocity
+    position = circle_pos,  # Initial position
+    acceleration = (0, 10),  # Initial acceleration
+    force = (0, 0)  # Initial force
+)
+
+# Create a Physics instance
+physics = Physics()
+
+
 # Main loop
 running = True
 while running:
@@ -28,10 +44,15 @@ while running:
     screen.fill(BLACK)
     
     # Draw the solid circle
-    pygame.draw.circle(screen, WHITE, circle_pos, circle_radius)
+    pygame.draw.circle(screen, first_body.color, first_body.position, first_body.radius)
     
     # Update display
     pygame.display.flip()
+    
+    # physics.calc_accelaration_by_force(first_body)
+    physics.calc_new_position_and_velocity(first_body, 0.01)  # Update position with a time step of 0.01 seconds
+    
+    pygame.time.delay(10)
 
 # Quit pygame
 pygame.quit()
--- physics.py
+++ physics.py
@@ -6,6 +6,9 @@ class Physics:
         self.universal_grav_const = 6.67430e-11  # Universal gravitational constant in m^3 kg^-1 s^-2
         self.meter_to_pixel = 100  # Conversion factor from meters to pixels 1 m = 100 pixels
     
+    def to_pixel(self, value: float) -> float:
+        return value * self.meter_to_pixel
+    
     def calc_accelaration_by_force(self, body: Body) -> None:
         if body.mass <= 0.0:
             raise ValueError("Mass must be greater than zero.")
@@ -17,6 +20,21 @@ class Physics:
             raise ValueError("Time must be greater than zero.")
         else:
             # Calculate new postion using the formula: s = ut + 1/2 at^2
-            new_x = body.position[0] + body.velocity[0] * time + 0.5 * body.acceleration[0] * time**2
-            new_y = body.position[1] + body.velocity[1] * time + 0.5 * body.acceleration[1] * time**2
+            dis_x = body.velocity[0] * time + 0.5 * body.acceleration[0] * time**2
+            dis_y = body.velocity[1] * time + 0.5 * body.acceleration[1] * time**2
+            new_x = body.position[0] + self.to_pixel(dis_x)
+            new_y = body.position[1] + self.to_pixel(dis_y)
             body.position = (new_x, new_y)
+            
+    def calc_new_velocity(self, body: Body, time: float) -> None:
+        if time <= 0.0:
+            raise ValueError("Time must be greater than zero.")
+        else:
+            # Calculate new velocity using the formula: v = u + at
+            v_x = body.velocity[0] + body.acceleration[0] * time
+            v_y = body.velocity[1] + body.acceleration[1] * time
+            body.velocity = (v_x, v_y)
+    
+    def calc_new_position_and_velocity(self, body: Body, time: float) -> None:
+        self.calc_new_position(body, time)
+        self.calc_new_velocity(body, time)
